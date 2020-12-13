4010188bcb5488bbedb3b229da67836bb78d774a
Gabriel Gomes <gabrielsogom@gmail.com>
2020-12-13 12:29:42 -0300

    Primeira Versao

---
+++ HELP.md
@@ -0,0 +1,8 @@
+
+
+npm install react-router-dom
+
+
+
+
+yarn add react-router-dom
--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+
+import {BrowserRouter as Router,Link, Switch,Route,Redirect} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Redirect from='/' to='/Home'/>
+
+      <Link to="/Home">Home</Link>
+      <p></p>
+      <Link to="/AboutUs">About Us</Link>
+      <Switch>
+
+        <Route path="/Home">
+            <div>
+              Home
+            </div>
+        </Route>
+
+        <Route path="/AboutUs">
+            <div>
+              About Us
+            </div>
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
